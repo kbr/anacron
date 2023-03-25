0da6733895207a3a50374898869083768d08f697
Klaus Bremer <bremer@bremer-media.com>
2023-03-25 12:41:47 +0100

    configuration._get_db_path added

--- anacron/configuration.py
+++ anacron/configuration.py
@@ -26,13 +26,29 @@ class Configuration:
     for better testing.
     """
     def __init__(self, db_filename=DB_FILE_NAME):
-        self.db_path = pathlib.Path(".").parent
+        self.db_path = self._get_db_path()
         self.db_filename = db_filename
         self.monitor_idle_time = MONITOR_IDLE_TIME
         self.worker_idle_time = WORKER_IDLE_TIME
         self.is_active = False
         if DJANGO_IS_INSTALLED:
             self.is_active = not settings.DEBUG
+
+    def _get_db_path(self):
+        """
+        Return the directory as a Path object, where the anacron
+        database (and also the test-database) get stored.
+        """
+        try:
+            home_dir = pathlib.Path().home()
+        except RuntimeError:
+            # can't resolve homedir, take the present working
+            # directory. Depending on the application .gitignore
+            # should get extended with a ".anacron/*" entry.
+            home_dir = self.cwd
+        home_dir = home_dir / ".anacron"
+        home_dir.mkdir(exist_ok=True)
+        return home_dir
 
     @property
     def db_file(self):
