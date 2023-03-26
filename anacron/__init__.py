2fe1cd553f04fe5bc38743cea15b7cd39ba0913a
Klaus Bremer <bremer@bremer-media.com>
2023-03-26 19:47:46 +0200

    missing engine import added

--- anacron/__init__.py
+++ anacron/__init__.py
@@ -6,5 +6,7 @@ simple background task handling with no dependencies.
 
 # import shortcuts:
 from .decorators import cron, delegate
+# start engine depending on configuration
+import .engine
 
 __version__ = "0.1.dev"
